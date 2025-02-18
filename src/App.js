e890a7e99d0079127c610d139df3be208626d63e
navaneethanp7604 <navaneethanp7604@gmail.com>
2025-02-18 18:17:25 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import Home from "./Home";
 
+
 function App() {
   return (
     <div className="App">
--- src/Home.js
+++ src/Home.js
@@ -1,16 +1,21 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
+import './Home.css';
+import logo from './images/logo.png'
 
 const Home = () => {
     return(
-        <nav>
-            <header>service</header>
-            <li>
-                <ul className="about">about</ul>
-                <ul className="services">services</ul>
-                <ul className="contact">contact</ul>
-                <ul className="products">products</ul>
-            </li>
+        <nav  className="navbar">
+            <header>
+            <img src={logo} alt="Navan Mobile Logo" className="logo" />
+                service
+            </header>
+            <div>
+                <button className="about">about</button>
+                <button className="services">services</button>
+                <button className="contact">contact</button>
+                <button className="products">products</button>
+            </div>
         </nav>
     );
 };
