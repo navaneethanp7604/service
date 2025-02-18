f00d813afcebeae069211f90265250f8d5017366
navaneethanp760 <navanethanp7604@gmail.com>
2025-02-18 12:54:18 +0530

    second commit

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+
+const Home = () => {
+    return(
+        <nav>
+            <header>service</header>
+            <li>
+                <ul className="about">about</ul>
+                <ul className="services">services</ul>
+                <ul className="contact">contact</ul>
+                <ul className="products">products</ul>
+            </li>
+        </nav>
+    );
+};
+
+export default Home;
